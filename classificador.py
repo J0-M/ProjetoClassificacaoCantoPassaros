d1ab20a5abdaa572df2a307881be73600a94bc5c
J0-M <joaomarcosavancini00@gmail.com>
2025-04-23 15:48:27 -0300

    Implementado validação Cruzada

--- classificador.py
+++ classificador.py
@@ -2,11 +2,10 @@ import os
 import numpy as npy
 import pandas as pd
 import pickle
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import f1_score, classification_report
+from sklearn.metrics import accuracy_score, f1_score, classification_report, get_scorer_names, get_scorer
 
 def melhorK(ks, X_treino, X_val, y_treino, y_val, X_teste, y_teste):
     acuracias_val = []
@@ -26,34 +25,16 @@ def melhorK(ks, X_treino, X_val, y_treino, y_val, X_teste, y_teste):
     pred = knn.predict(X_teste)
     print("acurácia no teste: %.2f" % (accuracy_score(y_teste, pred)))
     
-    with open("melhorK.pkl", "wb") as file:
-        pickle.dump(melhor_k, file)
+    #with open("melhorK_knn.pkl", "wb") as file:
+        #pickle.dump(melhor_k, file)
     
-    return melhor_k
+    return knn, melhor_k, melhor_val
 
-def main():
-    dataframePath = "dataframeSegmentado.pkl"
-
-    if os.path.exists(dataframePath):
-        with open("dataframeSegmentado.pkl", "rb") as readFile:
-            df = pickle.load(readFile)
-            print("Dataframe carregado com sucesso!")
-
-    X = df.drop(columns=["roi_label"]) # x = features
-    y = df["roi_label"] # y = passaros
-
-    print(X.shape)
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2000) #80% treino, 20% teste
-
-    scaler = StandardScaler()
-    X_train_scaled = scaler.fit_transform(X_train) # normaliza os dados das features para que o treino não seja enviesado por valores distoantes
-    X_test_scaled = scaler.transform(X_test)
-    
-    melhorKPath = "melhorK.pkl"
+def calculaK(X, y):
+    melhorKPath = "melhorK_knn.pkl"
     
     if os.path.exists(melhorKPath):
-        with open("melhorK.pkl", "rb") as readFile:
+        with open("melhorK_knn.pkl", "rb") as readFile:
             k = pickle.load(readFile)
             print(f"K carregado, k = {k}")
     else:
@@ -67,11 +48,12 @@ def main():
         X_teste = ss.transform(X_teste)
         X_val = ss.transform(X_val)
         
-        k = melhorK(ks, X_treino, X_val, y_treino, y_val, X_teste, y_teste)
+        _, k, _ = melhorK(ks, X_treino, X_val, y_treino, y_val, X_teste, y_teste)
         print(f"k = {k}")
     
-    #####################################
+    return k
 
+def treinaKNN(k, X_train_scaled, y_train):
     knnPath = "knnModel.pkl"
     
     if os.path.exists(knnPath):
@@ -85,14 +67,115 @@ def main():
 
         print("Modelo salvo como knn_model.pkl")
 
+    knn = KNeighborsClassifier(n_neighbors=k)
     knn.fit(X_train_scaled, y_train)
+    
+    return knn
 
+def printResultados(knn, X_test_scaled, y_test):
     y_pred = knn.predict(X_test_scaled) #testa o knn com o conjunto 20% teste
 
     f1 = f1_score(y_test, y_pred, average="weighted") #f1 score
         
     print(f"F1-score do KNN: {f1:.2f}")
     print(classification_report(y_test, y_pred))
+    
+def knnCruzado(X, y):
+    #a validação cruzada será realizada em 10 vias.
+    k_vias = 10
+
+    #usar o protocolo de validação cruzada estratificada
+    skf = StratifiedKFold(n_splits=k_vias, shuffle=True, random_state=10)
+
+    acuracias = []
+
+    #a função split retorna os índices das instâncias que devem ser usadas para o treinamento e o teste.
+    for idx_treino, idx_teste in skf.split(X, y):
+        
+        #extrair as instâncias de treinamento de acordo com os índices fornecidos pelo skf.split
+        X_treino = X.iloc[idx_treino]
+        y_treino = y.iloc[idx_treino]
+        
+        #extrair as instâncias de teste de acordo com os índices fornecidos pelo skf.split
+        X_teste = X.iloc[idx_teste]
+        y_teste = y.iloc[idx_teste]
+        
+        #filtra os y, e elimina as amostras que tem menos que 2 especies
+        counts = y_treino.value_counts()
+        classes_validas = counts[counts >= 2].index
+        filtro = y_treino.isin(classes_validas)
+        X_treino = X_treino[filtro]
+        y_treino = y_treino[filtro]
+        
+        #separar as instâncias de treinamento entre treinamento e validação para a otimização do hiperparâmetro k
+        X_treino, X_val, y_treino, y_val = train_test_split(X_treino, y_treino, test_size=0.2, stratify=y_treino, shuffle=True, random_state=10)
+        
+        #colocar todas as variáveis na mesma escala, usando o conjunto de treinamento para calcular os parâmetros da escala
+        ss = StandardScaler()
+        ss.fit(X_treino)
+        X_treino = ss.transform(X_treino)
+        X_teste = ss.transform(X_teste)
+        X_val = ss.transform(X_val)
+
+        #escolher o k com o melhor resultado no conjunto de validação e treinar o KNN com o melhor k.
+        knn, _, _ = melhorK(range(1,30,2), X_treino, X_val, y_treino, y_val, X_teste, y_teste)
+        
+        y_pred = knn.predict(X_teste)
+        
+        #calcular a acurácia no conjunto de testes desta iteração e salvar na lista.
+        
+        #print(get_scorer_names())
+        acuracias.append(accuracy_score(y_teste, knn.predict(X_teste)))
+        
+    
+    return knn, acuracias
+    
+
+def main():
+    dataframePath = "dataframeSegmentado.pkl"
+
+    if os.path.exists(dataframePath):
+        with open("dataframeSegmentado.pkl", "rb") as readFile:
+            df = pickle.load(readFile)
+            print("Dataframe carregado com sucesso!")
+
+    X = df.drop(columns=["roi_label"]) # x = features
+    y = df["roi_label"] # y = passaros
+
+    print(X.shape)
+    
+    #####################################
+
+    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2000) #80% treino, 20% teste
+
+    #scaler = StandardScaler()
+    #X_train_scaled = scaler.fit_transform(X_train) # normaliza os dados das features para que o treino não seja enviesado por valores distoantes
+    #X_test_scaled = scaler.transform(X_test)
+    
+    #####################################
+    
+    #k = calculaK(X, y)
+    #knn = treinaKNN(k, X_train_scaled, y_train)
+
+    #printResultados(knn, X_test_scaled, y_test)
+    
+    #####################################
+    
+    knnCruzadoPath = "knnCruzado.pkl"
+    acuraciasPath = "acuracias.pkl"
+    
+    if os.path.exists(knnCruzadoPath) and os.path.exists(acuraciasPath):
+        with open("knnCruzado.pkl", "rb") as readFile:
+            knn = pickle.load(readFile)
+            print("KNN com validação cruzada carregado com sucesso!")
+            
+        with open("acuracias", "rb") as readFile:
+            acuracias = pickle.load(readFile)
+            print("Acuracias carregados com sucesso!")
+    else:
+        knn, acuracias = knnCruzado(X, y)
+    
+    print("min: %.2f, max: %.2f, avg +- std: %.2f+-%.2f" % (min(acuracias), max(acuracias), npy.mean(acuracias), npy.std(acuracias)))
 
     
 if __name__ == '__main__':
